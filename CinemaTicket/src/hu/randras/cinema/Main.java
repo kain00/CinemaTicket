dda3abd5c67de03b3541d74e72facfa73a4f4786
kain00 <andris.romhanyi@gmai.com>
2011-11-01 18:01:27 +0100

    basic project

---
+++ CinemaTicket/src/hu/randras/cinema/Main.java
@@ -0,0 +1,10 @@
+package hu.randras.cinema;
+
+import hu.randras.cinema.view.MainFrame;
+
+
+public class Main {
+	public static void main(String[] args) {
+		new MainFrame().setVisible(true);
+	}
+}
---
+++ CinemaTicket/src/hu/randras/cinema/view/MainFrame.java
@@ -0,0 +1,7 @@
+package hu.randras.cinema.view;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	
+}
